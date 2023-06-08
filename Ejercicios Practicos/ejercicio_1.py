23a3809699a8cf68f523361a6bbc930d984d6474
Samuel Calderon <samuelcalderon1202@hotmail.com>
2023-06-07 21:11:59 -0500

    ejercicios

--- Ejercicios Practicos/ejercicio_1.py
+++ Ejercicios Practicos/ejercicio_1.py
@@ -11,9 +11,11 @@ diferencia_min = 100 - (curso_tomado / curso_min * 100)
 diferencia_max = 100 - (curso_tomado * 1000 // curso_max / 10)
 diferencia_promedio = 100 - (curso_tomado / promedio_cursos * 100)
 
+print('------------------------')
 print(f'El curso tomado tarda {diferencia_min}% menos que el más rapido')
 print(f'El curso tomado tarda {diferencia_max}% menos que el más lento')
 print(f'El curso tomado tarda {diferencia_promedio}% menos que el más promedio')
+
 
 #Material Inservible
 
@@ -25,9 +27,14 @@ crudo_promedio_cursos = 5
 material_reducido = 100 - (promedio_cursos / crudo_promedio_cursos * 100)
 material_reducido_tomado = 100 - (curso_tomado * 1000 // crudo_curso_tomado / 10)
 
+print('------------------------')
 print(f'El material inservible promedio reducido es de {material_reducido}%')
 print(f'El material inservible promedio reducido en el curso tomado es de {material_reducido_tomado}%')
+print('------------------------')
 
 #Ver 10 horas curso
 
-#si 1.5
+print(f'Ver 10 horas de este curso equivale a ver {promedio_cursos * 100 // curso_tomado / 10} horas de otros cursos')
+print(f'Ver 10 horas de otros cursos equivale a ver {curso_tomado * 100 // promedio_cursos / 10} horas de este curso')
+print('------------------------')
+
---
+++ Ejercicios Practicos/ejercicio_2.py
@@ -0,0 +1,13 @@
+texto_real = input ("Por favor ingrese un texto real que diria: ")
+cantidad_palabras = len(texto_real.split())
+tiempo_en_decir = cantidad_palabras / 2
+
+print(f'El tiempo que tardaria en decir toda la frase es: {tiempo_en_decir} seg')
+print(f'La cantidad de palabras que dijo fue: {cantidad_palabras}')
+
+if tiempo_en_decir > 60:
+    print("Para flaco tampoco te pedi un testamento")
+else:
+    print('Que rapido que hablas')
+    
+print (f"Dalto lo diria en {cantidad_palabras/2*0.3} segundos en decirlo")
