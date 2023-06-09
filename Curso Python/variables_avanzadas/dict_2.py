fa766590c266b049ee71bda4d5b9c515398951c4
Samuel Calderon <samuelcalderon1202@hotmail.com>
2023-06-09 12:39:44 -0500

    Aprendieno10

---
+++ Curso Python/variables_avanzadas/dict_2.py
@@ -0,0 +1,12 @@
+#creando diccionario con dict ()
+diccionario = dict (nombre="Samuel",apellido="Calderon")
+
+#las listas no pueden ser claves y se usa frozenset para meter conjuntos
+diccionario = {frozenset(["Samuel","aburrido"]):"djasjds"}
+
+#diccionario con fromkeys(), valores indefinidos
+#primer dato, lo que se va a iterar, segundo dato, lo que se va a igualar
+diccionario = dict.fromkeys(["nombre","apellido"])
+
+diccionario = dict.fromkeys(["nombre","apellido"],"No se")
+print(diccionario)
