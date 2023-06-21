ac4302ca199f117192816e976676e5be9d16d138
Samuel Calderon <samuelcalderon1202@hotmail.com>
2023-06-21 18:00:11 -0500

    ejercicio5

---
+++ Ejercicios Practicos/ejercicio5.py
@@ -0,0 +1,13 @@
+#serie de fibonacci hasta el numero que queremos
+def fibonnacci (num):
+    serie = [0,1]
+    print (serie[-1])
+    print (serie [-2])
+    for i in range (num):
+        i = serie[-1] + serie[-2]
+        serie.append(i)
+    return serie
+        
+resultado = fibonnacci(121)
+print (resultado)
+    
--- Ejercicios Practicos/ejercicio_4.py
+++ Ejercicios Practicos/ejercicio_4.py
@@ -1,9 +1,10 @@
-
+#funcionq ue verifica si el numero es primo
 def es_primo (num_max):
     for i in range(2,num_max-1):
         if num_max % i == 0: return False
     return True
 
+#funcion que nos imprime los numeros primos
 def primos_hasta (num_max):
     primos = []
     for i in range (3,num_max):
