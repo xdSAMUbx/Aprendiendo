3c1bffaa6e82e9716c1746e8d0a69af568584c03
Samuel Calderon <samuelcalderon1202@hotmail.com>
2023-06-20 19:41:51 -0500

    Ejercicio3

---
+++ Ejercicios Practicos/ejercicio_4.py
@@ -0,0 +1,13 @@
+#Numeros primos hasta llegar a ese numero
+num_max = int(input("Ingrese el numero hasta el cual desea conocer sus numeros primos: "))
+
+def encontrar_primos (num_max):
+    primos = []
+    for i in range(1,num_max):
+        if num_max % i == 0 and  :
+            primos.append(i)
+            
+        print(primos)
+        
+encontrar_primos(num_max)
+            
