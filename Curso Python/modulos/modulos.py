5919e41720050b07879eb260cf0247b49aa4dcf4
Samuel Calderon <samuelcalderon1202@hotmail.com>
2023-06-21 18:32:27 -0500

    modulos final

--- Curso Python/modulos/modulos.py
+++ Curso Python/modulos/modulos.py
@@ -1,14 +1,23 @@
 #asignamos el nombre que queramos al modulo
-#import modulo_saludar as m_saludar 
+import modulo_saludar as m_saludar 
 
 #Desde ese modulo se importa la funcion
 #from modulo_saludar import saludar
 
 #Desde ese modulo importamos la funcion y le cambiamos el nombre
-from modulo_saludar import saludar as saludo_diario
+#from modulo_saludar import saludar as saludo_diario
 
 #Importar todo lo de un modulo, no recomendable
-from modulo_saludar import *
+#from modulo_saludar import *
 
-saludo = saludo_diario("Samuel")
+saludo = m_saludar.saludar("Samuel")
 print(saludo)
+
+#propiedades y metodos del namespace
+#print(dir(m_saludar))
+
+#muestra el nombre del modulo que se importa
+#print(m_saludar.__name__)
+
+#el modulo que se ejecuta es este mismo (principal / main)
+print(__name__)
