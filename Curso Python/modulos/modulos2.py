b861fe8178c9cdd698119b342bbd16022d432a40
Samuel Calderon <samuelcalderon1202@hotmail.com>
2023-06-23 18:59:41 -0500

    modulos

---
+++ Curso Python/modulos/modulos2.py
@@ -0,0 +1,15 @@
+#si el modulo estuviera en una carpeta en la misma ruta
+#import funciones_buenas.saludar as m_saludar
+
+import sys
+
+#nos devuelve las propiedades de los modulos
+#print(sys.builtin_module_names)
+
+#nos devuelve la ruta de los modulos
+sys.path.append("D:\\Programacion\\aprendiendo_python\\Curso Python\\funciones_buenas")
+print(sys.path)
+
+import saludar
+
+print(saludar.saludar("Samuel"))
