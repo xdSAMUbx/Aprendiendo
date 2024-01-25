b3c4135b0f22b6e2516f50c4c61347c91ca275c4
Samuel Calderon <samuelcalderon1202@hotmail.com>
2024-01-25 17:26:27 -0500

    ejercicio2

--- Ejercicios Practicos/ejer_leet_code_2.py
+++ Ejercicios Practicos/ejer_leet_code_2.py
@@ -1,3 +1,10 @@
-mtz = [[1,2,3],[3,2,1]]
-for fila in mtz:
-    print(fila)
+n = 14
+cont = 0
+while n!=0:
+    if n%2 == 0:
+        n = n/2
+        cont += 1 
+    else:
+        n -= 1
+        cont += 1 
+print(cont)
